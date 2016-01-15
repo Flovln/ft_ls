6ac3007ca5df3c0ae49c3c334717c46800c4f0ad
Florent VIOLIN <fviolin@e3r5p21.42.fr>
2016-01-15 15:52:21 +0100

    more libraries

--- ft_ls.h
+++ ft_ls.h
@@ -6,8 +6,17 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 11:41:51 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/15 11:41:53 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/15 15:52:04 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LS_H
+# define FT_LS_H
 
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include ".libft/libft.h"
+
+#endif
