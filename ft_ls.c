af2702a2662c87372ee992702916e41fee0229cd
Florent VIOLIN <fviolin@e2r5p19.42.fr>
2016-01-21 17:04:09 +0100

    backup / work in progress

--- ft_ls.c
+++ ft_ls.c
@@ -6,12 +6,12 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 12:21:06 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/21 16:26:26 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/21 17:03:42 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
+/*
 t_file_info		*ft_init_file_info(t_file_info *get_info)
 {
 	get_info->mode = 0;
@@ -20,13 +20,27 @@ t_file_info		*ft_init_file_info(t_file_info *get_info)
 	get_info->gid = 0;
 	return (get_info);
 }
-/*
+*/
 static void		ft_file_stat(struct stat *get_stat, t_file_info *get_info)
 {
-	get_info->uid = &get_stat->st_uid;
-	printf("Valeur uid : %d\n", get_info->uid);	
+
+	/*
+	struct passwd *pwd;
+
+	pwd = NULL;
+	if ((pwd = getpwuid(get_stat->st_uid)) != NULL)
+	{		
+		get_info->uid = pwd->pw_name;
+	}
+	printf("Valeur uid : %s\n", get_info->uid);	*/
 }
-*/
+
+static void		ft_list_files(struct dirent *read_ptr)
+{
+	if (ft_strncmp(read_ptr->d_name, ".", 1) != 0)
+		ft_putendl(read_ptr->d_name);
+}
+
 int		main(int ac, char **av)
 {
 	DIR				*dir_ptr;
@@ -44,10 +58,10 @@ int		main(int ac, char **av)
 	{
 		if (stat(av[1], get_stat) > 0)
 			return (1);
+//		get_info = malloc(sizeof(t_file_info));
 //		get_info = ft_init_file_info(get_info);
-//		ft_file_stat(get_stat, get_info);
-		if (ft_strncmp(read_ptr->d_name, ".", 1) != 0)
-			ft_putendl(read_ptr->d_name);
+		ft_file_stat(get_stat, get_info);
+		ft_list_files(read_ptr); //affiche les fichiers + dossiers du repertoire courant
 	}
 	closedir (dir_ptr);
 	return (0);
