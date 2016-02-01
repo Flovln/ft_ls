6a616a17232898bf535d7ed9635e1e86a5178b7a
Florent VIOLIN <fviolin@e3r12p6.42.fr>
2016-02-01 17:29:45 +0100

    Norme fixed / backup / work in progress

--- ft_ls.h
+++ ft_ls.h
@@ -6,55 +6,56 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 11:41:51 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/01 16:59:55 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/02/01 17:29:09 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include <pwd.h>
-#include <uuid/uuid.h>
-#include <dirent.h> // opendir readdir closedir
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <grp.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <dirent.h> // opendir readdir closedir
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <grp.h>
+# include <time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
 /*
-typedef struct 		s_opt
+typedef struct 				s_opt
 {
-	int				l;
-	int				R;
-	int				a;
-	int				r;
-	int				t;
-}					t_opt;
+	int						l;
+	int						R;
+	int						a;
+	int						r;
+	int						t;
+}							t_opt;
 */
 
 typedef struct				s_data
 {
 	char					get_perm[11];
-   	char					*links; // nlink_t nb de fichiers/sous dossiers/fichiers
-   	char					*uid; // owner name
-   	char					*gid; // group name
-   	char					*size; // off_t taille en octets du fichier/dossier
+	char					*links;
+	char					*uid;
+	char					*gid;
+	char					*size;
 }							t_data;
 
 typedef struct				s_lst
 {
 //	char					*path;
+	t_data					*file_data;
 	char					*date;
 	char					*name;
-	t_data					*file_data;
 	struct s_lst			*next;
 }							t_lst;
 
-void		ft_perm_acc(t_lst *elem, struct stat *file_stat);
-void		ft_add_node(t_lst *head, t_lst *new_node);
-t_lst		*ft_get_data(t_lst *head, char *path, char *file);
+void						ft_perm_acc(t_lst *elem, struct stat *file_stat);
+void						ft_add_node(t_lst *head, t_lst *new_node);
+t_lst						*ft_get_data(t_lst *head, char *path, char *file);
 
 #endif
