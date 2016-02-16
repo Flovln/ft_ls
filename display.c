be1851c5fb8a4c4af28c861205226f50a1f00d36
Florent VIOLIN <fviolin@e1r5p21.42.fr>
2016-02-16 14:52:56 +0100

    modification ft_display_curr

--- display.c
+++ display.c
@@ -6,27 +6,31 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 17:59:52 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/11 14:52:39 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/02/16 14:16:30 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	ft_display_curr(t_lst **node, int i)
+void	ft_display_curr(t_lst *node, int i)
 {
-	if (i)
-		while (*node)
+	if (!i)
+	{
+		while (node)
 		{
-			ft_putendl((*node)->name);
-			*node = (*node)->next;
+			if (ft_strncmp(node->name, ".", 1))
+				ft_putendl(node->name);
+			node = node->next;
 		}
+	}
 	else
-		while (*node)
+	{
+		while (node)
 		{
-			if (ft_strncmp((*node)->name, ".", 1) != 0)
-				ft_putendl((*node)->name);
-			*node = (*node)->next;
+			ft_putendl(node->name);
+			node = node->next;
 		}
+	}
 }
 
 void	ft_display_l(t_lst **node, int i)
