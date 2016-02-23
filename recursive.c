e0b641d6ede2c1df8f0761bc63d3c4c7ce906874
Florent VIOLIN <fviolin@e1r1p12.42.fr>
2016-02-23 10:49:27 +0100

    backup / working on memory leaks + norme

--- recursive.c
+++ recursive.c
@@ -6,7 +6,7 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/15 13:28:41 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/22 14:08:06 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/02/23 10:48:52 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,44 +31,13 @@ static	void	ft_put_css(char c, char *s1, char *s2)
 	ft_putchar(c);
 	ft_putstr(s1);
 	ft_putstr(s2);
-//	ft_read_param(ft_strjoin(path, ft_add_slash(all_d[i])), opt);
-}
-
-static void		ft_recursive_r(t_opt *opt, char *path, int nb_d, char **all_d)
-{
-	int		j;
-	int		i;
-
-	if (opt->r == 0)
-	{
-		i = -1;
-		while (++i < nb_d)
-		{
-			if (all_d[i])
-			{
-				ft_put_css('\n', ft_strjoin(path, all_d[i]), ":\n");
-				ft_read_param(ft_strjoin(path, ft_add_slash(all_d[i])), opt);
-			}
-		}
-	}
-	else if (opt->r == 1)
-	{
-		j = nb_d;
-		while (--j > -1)
-		{
-			if (all_d[j])
-			{
-				ft_put_css('\n', ft_strjoin(path, all_d[j]), ":\n");
-				ft_read_param(ft_strjoin(path, ft_add_slash(all_d[j])), opt);
-			}
-		}
-	}
 }
 
 void			ft_recursive(t_lst *node, t_opt *opt, char *path, int nb_dir)
 {
 	char	**all_dir;
 	int		i;
+	int		j;
 
 	i = 0;
 	if (!(all_dir = (char **)malloc(sizeof(char *) * nb_dir + 1)))
@@ -91,5 +60,29 @@ void			ft_recursive(t_lst *node, t_opt *opt, char *path, int nb_dir)
 		node = node->next;
 	}
 	all_dir[i] = NULL;
-	ft_recursive_r(opt, path, nb_dir, all_dir);
+	if (opt->r == 0)
+	{
+		i = -1;
+		while (++i < nb_dir)
+		{
+			if (all_dir[i])
+			{
+				ft_put_css('\n', ft_strjoin(path, all_dir[i]), ":\n");
+				ft_read_param(ft_strjoin(path, ft_add_slash(all_dir[i])), opt);
+			}
+		}
+	}
+	else if (opt->r == 1)
+	{
+		j = nb_dir;
+		while (j > -1)
+		{
+			if (all_dir[j])
+			{
+				ft_put_css('\n', ft_strjoin(path, all_dir[j]), ":\n");
+				ft_read_param(ft_strjoin(path, ft_add_slash(all_dir[j])), opt);
+			}
+			j--;
+		}
+	}
 }
