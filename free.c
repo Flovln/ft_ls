90376b4e24b2362a84b090b6d1fcdc0001ec933c
Florent VIOLIN <fviolin@e3r7p6.42.fr>
2016-02-25 19:25:46 +0100

    new file for free functions

---
+++ free.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/25 17:01:52 by fviolin           #+#    #+#             */
+/*   Updated: 2016/02/25 17:05:47 by fviolin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void		ft_free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		tab[i] = NULL;
+		i++;
+	}
+}
+
+void 	ft_free_data(t_lst **node)
+{
+	if ((*node)->file_data->links)
+		ft_strdel(&(*node)->file_data->links);
+	if ((*node)->file_data->uid)
+		ft_strdel(&(*node)->file_data->uid);
+	if ((*node)->file_data->gid)
+		ft_strdel(&(*node)->file_data->gid);
+	if ((*node)->file_data->size)
+		ft_strdel(&(*node)->file_data->size);
+	if ((*node)->file_data->min)
+		ft_strdel(&(*node)->file_data->min);
+	if ((*node)->file_data->maj)
+		ft_strdel(&(*node)->file_data->maj);
+	if ((*node)->file_data->maj_min)
+		ft_strdel(&(*node)->file_data->maj_min);
+	if ((*node)->date)
+		ft_strdel(&(*node)->date);
+	if ((*node)->name)
+		ft_strdel(&(*node)->name);
+	//	(*node)->file_data->blocks = 0;
+}
+
+void	ft_free_list(t_lst **node)
+{
+	int i;// test printf
+
+	i = 0;// test
+	while (*node)
+	{
+//		printf("Node freed nb = |%d|  name : %s\n", i, (*node)->name);
+		ft_free_data(node);
+		*node = (*node)->next;
+//		i++; //test 
+	}
+	free((*node));
+	node = NULL;
+}
--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/20 15:15:35 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/22 14:55:00 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/02/25 17:05:57 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,56 +49,4 @@ void		ft_tab_swap(char **s1, char **s2)
 	tmp = *s1;
 	*s1 = *s2;
 	*s2 = tmp;
-}
-
-void		ft_free_tab(char **tab)
-{
-	int	i;
-
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		tab[i] = NULL;
-		i++;
-	}
-}
-
-void 	ft_free_data(t_lst **node)
-{
-	if ((*node)->file_data->links)
-		ft_strdel(&(*node)->file_data->links);
-	if ((*node)->file_data->uid)
-		ft_strdel(&(*node)->file_data->uid);
-	if ((*node)->file_data->gid)
-		ft_strdel(&(*node)->file_data->gid);
-	if ((*node)->file_data->size)
-		ft_strdel(&(*node)->file_data->size);
-	if ((*node)->file_data->min)
-		ft_strdel(&(*node)->file_data->min);
-	if ((*node)->file_data->maj)
-		ft_strdel(&(*node)->file_data->maj);
-	if ((*node)->file_data->maj_min)
-		ft_strdel(&(*node)->file_data->maj_min);
-	if ((*node)->date)
-		ft_strdel(&(*node)->date);
-	if ((*node)->name)
-		ft_strdel(&(*node)->name);
-//	(*node)->file_data->blocks = 0;
-}
-
-void	ft_free_list(t_lst **node)
-{
-	int i;// test printf
-
-	i = 0;// test
-	while (*node)
-	{
-//		printf("Node freed nb = |%d|  name : %s\n", i, (*node)->name);
-		ft_free_data(node);
-		*node = (*node)->next;
-		i++; //test 
-	}
-	free((*node));
-	node = NULL;
 }
