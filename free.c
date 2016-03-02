e121ac29edecfec53c1f440a0275019a1afb2935
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2016-03-02 15:25:26 +0100

    Norme OK / backup

--- free.c
+++ free.c
@@ -6,13 +6,13 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 17:01:52 by fviolin           #+#    #+#             */
-/*   Updated: 2016/02/26 17:08:13 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/03/02 15:25:07 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void		ft_free_tab(char **tab)
+void	ft_free_tab(char **tab)
 {
 	int	i;
 
@@ -25,7 +25,7 @@ void		ft_free_tab(char **tab)
 	}
 }
 
-void 	ft_free_data(t_lst **node)
+void	ft_free_data(t_lst **node)
 {
 	if ((*node)->file_data->links)
 		ft_strdel(&(*node)->file_data->links);
@@ -39,21 +39,18 @@ void 	ft_free_data(t_lst **node)
 		ft_strdel(&(*node)->file_data->maj);
 	if ((*node)->file_data->size)
 		ft_strdel(&(*node)->file_data->size);
-	//if ((*node)->file_data->maj_min)
 	else
 		ft_strdel(&(*node)->file_data->maj_min);
 	if ((*node)->date)
 		ft_strdel(&(*node)->date);
 	if ((*node)->name)
 		ft_strdel(&(*node)->name);
-	//	(*node)->file_data->blocks = 0;
 }
 
 void	ft_free_list(t_lst **node)
 {
 	while (*node)
 	{
-//		printf("Node freed nb = |%d|  name : %s\n", i, (*node)->name);
 		ft_free_data(node);
 		*node = (*node)->next;
 	}
